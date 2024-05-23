2bd07037ff35e035b8652c61cbffd681e1e1dd7a
Avi <avicreationstudio@gmail.com>
2024-05-23 15:28:17 +0530

    changed app.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          my name is Khan
         </a>
       </header>
     </div>
